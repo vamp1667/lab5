b58af0fd4febbf157f9d02fe1dbd8453540d4ab7
21648 <21648@1k129s3.dune.edu>
2024-03-20 08:36:16 +0200

    P3 hello

---
+++ src/lab4/Lab4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+/**
+ *
+ * @author 21648
+ */
+public class Lab4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello1");
+        // TODO code application logic here
+    }
+    
+}
