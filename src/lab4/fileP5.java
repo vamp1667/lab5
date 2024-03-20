7e0f5c0af726a6a1b3d74f6e286d2195634ab2f1
21648 <21648@1k129s3.dune.edu>
2024-03-20 08:47:29 +0200

    P5 commit file

---
+++ src/lab4/fileP5.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+/**
+ *
+ * @author 21648
+ */
+public class fileP5 {
+    
+}
